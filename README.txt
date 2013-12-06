75382d6ea064ba5127a61b762baa6c4acd994118
xochilpili <xochilpili@gmail.com>
2013-12-06 22:22:06 +0100

    Revision 2

--- README.txt
+++ README.txt
@@ -16,4 +16,20 @@ Tweak the config file as appropriate and then setup the application::
 
     paster setup-app config.ini
 
+This contains a MySQL Schema database stored in a file called : testDaniele.db.sql
+So, it's important to change the user && password --credentials-- to connect to your own mysqld (server)
+To do this please follow this instructions:
+
+	1.- Open controllers/formtest.py and edit the dns connection string
+		ie: mysql://user:password@localhost/testDaniele?charset=utf8&use_unicode=0', pool_recycle=3600
+		Edit this line in the file mentioned with your own credentials.
+
+	2.- Connect to your MySQL database engine and type :
+		mysql_shell>> source testDaniele.db.sql
+		this should be enough to have the database schema to run this application.
+ 
 Then you are ready to go.
+
+Thanks
+
+xOCh
--- danielechallenge/controllers/formtest.py
+++ danielechallenge/controllers/formtest.py
@@ -10,20 +10,20 @@ from sqlalchemy import *
 log = logging.getLogger(__name__)
 
 class FormtestController(BaseController):
-
+#begin
     def index(self):
 	str=""
 	return render('/derived/page/index.html', extra_vars={'result' : str})
 
     def contact(self):
 	return render('/derived/page/contact.html')
-
+#the form to add a new employee
     def form(self):
         # Return a rendered template
         #return render('/formtest.mako')
         # or, return a string
         return render('/derived/page/simpleform.html')
-
+#add new employees
     def submit(self):
 	#since i did not realize how to use the "connect" && "engine" global declared var in development.ini i have to use this every single time (fuck me)
 	str=""
@@ -38,12 +38,13 @@ class FormtestController(BaseController):
 	#return 'Your data is: %s' % nom + ' email: %s ' % mail + ' department: %s ' % dep + ' fech: %s ' % fech + ' Salary: %s ' % sal
 	str="New employee added!"
 	return render('/derived/page/index.html', extra_vars={'result' : str})
-
+#list the employees previously added
     def list(self):
 	str=""
 	counter=1
 	engine = create_engine('mysql://root:x0chetzalt@localhost/testDaniele?charset=utf8&use_unicode=0', pool_recycle=3600)
 	connection = engine.connect()
+	# i did not realize how can i convert date string from database to datetime object in python. Well i did it, but get some compilance error, so i prefer to do it from the query... 
 	result = engine.execute("""select cat_employees.nombre,cat_employees.email,date_format(cat_employees.fech_birth, '%%d/%%m/%%Y') as fech_birth, cat_employees.salario, cat_apartments.description from cat_employees inner join cat_apartments on cat_employees.id_apartment=cat_apartments.id_apartment order by cat_employees.nombre""")
 	for row in result:
 		str += h.literal('<tr><td> %s ' % row['nombre'] + '</td><td> %s ' % row['email']+'</td><td> %s' % row['description'] + '<td> %s ' % row['fech_birth'] + '</td><td> %s &#8364;' % row['salario'] + '</td></tr>') 
